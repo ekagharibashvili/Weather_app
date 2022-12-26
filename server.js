75e0a525473c5e292a2ed8a6fd06b40daaae32a0
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-12-26 13:37:53 +0400

    dontenv configuration

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const request = require("request");
-// https://api.openweathermap.org/data/2.5/weather?q=Tbilisi,+995&appid=5afdcbbb49b62e806381f7a8e0cb0e4d
+const dotenv = require("dotenv");
+
+
+// defining environment variables
+dotenv.config({
+  path: "./config.env",
+});
+
+// constants
+const PORT = process.env.PORT
+const SECRET_KEY = process.env.SECRET_KEY
+
 const app = express();
 
 app.get("/", (req, res) => {
   let cities = ["Tbilisi", "Batumi", "Kutaisi", "Rustavi", "Borjomi"];
   let city = req.query.city;
   request(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city},+995&appid=5afdcbbb49b62e806381f7a8e0cb0e4d`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},+995&appid=${SECRET_KEY}`,
     function (error, response, body) {
       let data = JSON.parse(body);
       if (response.statusCode === 200) {
@@ -21,7 +32,6 @@ app.get("/", (req, res) => {
           let weatherArr = [];
           for (let i = 0; i < cities.length; i++) {
             city = cities[i];
-            console.log(city);
             weatherArr.push(
               `The weather in "${city}" is ${data.weather[0].description}.`
             );
@@ -33,6 +43,6 @@ app.get("/", (req, res) => {
   );
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running...");
 });
