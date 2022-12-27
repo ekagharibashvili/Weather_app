90b61d9b6310acdf4ecc1be3411625b3fc9e42e2
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-12-27 13:40:39 +0400

    add last functionaliy

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -2,30 +2,45 @@ const citiesData = require('../data')
 const cities = citiesData.cities
 const fetch = require('node-fetch')
 const dotenv = require('dotenv')
-
 dotenv.config({
   path: './config.env'
 })
-
 const SECRET_KEY = process.env.SECRET_KEY
+// fetchUrls
+const urls = cities.map((city) => {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${city},+995&appid=${SECRET_KEY}`
+})
 
 exports.getWeather = async (req, res) => {
   try {
     const city = req.query.city
     const isIncluded = cities.includes(city)
+    // retrieve weather for one, supported city
     if (isIncluded) {
       const weather = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${city},+995&appid=${SECRET_KEY}`
       )
-      const response = await weather.json()
+      const data = await weather.json()
       res.status(200).json({
         status: 'OK',
-        data: response
+        data
       })
-    } else if (!isIncluded) {
+      // return result for unsupported city
+    } else if (city !== '' && !isIncluded) {
       res.status(404).json({
         status: 'Not Found',
         data: 'This city is not supported'
+      })
+      // retrieve weather of every city from array
+    } else {
+      const responsePromises = urls.map((url) => {
+        return fetch(url)
+      })
+      const responses = await Promise.all(responsePromises)
+      const data = await Promise.all(responses.map((response) => response.json()))
+      res.status(200).json({
+        status: 'OK',
+        data
       })
     }
   } catch (err) {
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const dotenv = require('dotenv')
 const weatherRouter = require('./routes/weatherRoutes')
 
-// defining environment variables
 dotenv.config({
   path: './config.env'
 })
@@ -13,36 +12,6 @@ const app = express()
 
 app.use('/', weatherRouter)
 
-/* app.get('/', (req, res) => {
-  let city = req.query.city
-  request(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city},+995&appid=${SECRET_KEY}`,
-    function (error, response, body) {
-      const data = JSON.parse(body)
-      if (response.statusCode !== 200) {
-        return error
-      } else {
-        if (cities.includes(city)) {
-          res.send(
-              `The weather in "${city}" is ${data.weather[0].description}`
-          )
-        } else if (city !== '' && !cities.includes(city)) {
-          res.send('This city is not supported')
-        } else {
-          const weatherArr = []
-          for (let i = 0; i < cities.length; i++) {
-            city = cities[i]
-            weatherArr.push(
-                `The weather in "${city}" is ${data.weather[0].description}.`
-            )
-          }
-          return res.send(weatherArr.join('<br>'))
-        }
-      }
-    }
-  )
-})
- */
 app.listen(PORT, () => {
   console.log('Server is running...')
 })
